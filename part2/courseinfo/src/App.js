0caa214149b94df703b00542a678e4d73e5287b7
Travis Stinebaugh <57374519+tstinebaugh@users.noreply.github.com>
2022-09-22 14:51:10 -0700

    step 7/8

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,26 +1,28 @@
 const Header = ({ name }) => <h1>{name}</h1>
 
-const Total = ({ sum }) => <p>Number of exercises {sum}</p>
+const Total = ({ sum }) => <b>Total of {sum} exercises</b>
 
-const Part = ({ part }) => 
+const Part = ({ part }) =>
   <p>
     {part.name} {part.exercises}
   </p>
 
-const Content = ({ parts }) => 
+const Content = ({ parts }) =>
   <>
-    {parts.map(part => 
+    {parts.map(part =>
       <Part key={part.id}
-        part={part} 
+        part={part}
       />
-    )}  
+    )}
   </>
 
 const Course = ({ course }) => {
-  return(
+  const sum = course.parts.reduce((total, value) => total + value.exercises, 0)
+  return (
     <>
-      <Header name = { course.name }></Header>
-      <Content parts = { course.parts }></Content>
+      <Header name={course.name}></Header>
+      <Content parts={course.parts}></Content>
+      <Total sum={sum}></Total>
     </>
   )
 }
@@ -44,6 +46,11 @@ const App = () => {
         name: 'State of a component',
         exercises: 14,
         id: 3
+      },
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4
       }
     ]
   }
