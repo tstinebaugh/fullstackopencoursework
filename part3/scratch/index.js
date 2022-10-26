cc7c674faa09874c14828a6c33304c57b6e784a9
Travis Stinebaugh <57374519+tstinebaugh@users.noreply.github.com>
2022-10-26 13:14:11 -0700

    ignored

--- part3/scratch/index.js
+++ part3/scratch/index.js
@@ -1,7 +1,19 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
 app.use(express.json())
+app.use(cors())
+
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+}
+
+app.use(requestLogger)
 
 const generateId = () => {
     const maxId = notes.length > 0
@@ -58,27 +70,33 @@ app.delete('/api/notes/:id', (request, response) => {
 })
   
 app.post('/api/notes', (request, response) => {
-const body = request.body
+    const body = request.body
 
-if (!body.content) {
-    return response.status(400).json({ 
-    error: 'content missing' 
-    })
-}
+    if (!body.content) {
+        return response.status(400).json({ 
+        error: 'content missing' 
+        })
+    }
 
-const note = {
-    content: body.content,
-    important: body.important || false,
-    date: new Date(),
-    id: generateId(),
-}
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        date: new Date(),
+        id: generateId(),
+    }
 
-notes = notes.concat(note)
+    notes = notes.concat(note)
 
-response.json(note)
+    response.json(note)
 })
 
-const PORT = 3001
-    app.listen(PORT, () => {
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+  }
+  
+  app.use(unknownEndpoint)
+
+const PORT = process.env.PORT || 3001
+  app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
