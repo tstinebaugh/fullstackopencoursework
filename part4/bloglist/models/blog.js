64f5557ea473819e9c10dade823e10a1c75eccb8
Travis Stinebaugh <57374519+tstinebaugh@users.noreply.github.com>
2022-12-22 14:39:23 -0800

    4.2

--- part4/bloglist/models/blog.js
+++ part4/bloglist/models/blog.js
@@ -7,19 +7,8 @@ const blogSchema = new mongoose.Schema({
   likes: Number
 })
 
-const url = process.env.MONGODB_URI
-console.log('connecting to', url)
-mongoose.connect(url)
-    .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
-
-
 blogSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
+    transform: (_document, returnedObject) => {
         returnedObject.id = returnedObject._id.toString()
         delete returnedObject._id
         delete returnedObject.__v
