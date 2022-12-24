4b7caf1edc5d1d5637f53abbf23932500187572d
Travis Stinebaugh <57374519+tstinebaugh@users.noreply.github.com>
2022-12-24 14:49:23 -0800

    4.4

--- part4/bloglist/tests/list_helper.test.js
+++ part4/bloglist/tests/list_helper.test.js
@@ -6,3 +6,57 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
 })
+
+describe('total likes', () => {
+  const listWithNoLikes = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      __v: 0
+    }
+  ]
+
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  const listWithMultipleLikes = [
+    ...listWithOneBlog,
+    {
+      likes: 1,
+    },
+    {
+      likes: 2,
+    },
+    {
+      likes: 3,
+    },
+    {
+      likes: 4,
+    }
+  ]
+
+  test('when list has only one blog, equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    expect(result).toBe(5)
+  })
+
+  test('blogs is nil', () => {
+    const result = listHelper.totalLikes(listWithNoLikes)
+    expect(result).toBe(0)
+  })
+
+  test('multiple blogs', () => {
+    const result = listHelper.totalLikes(listWithMultipleLikes)
+    expect(result).toBe(15)
+  })
+})
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,7 +1,16 @@
 const dummy = (blogs) => {
     return 1
 }
+
+const totalLikes = (blogs) => {
+    return (!blogs || blogs.length === 0) ? 
+    0 :
+    blogs.reduce((sum, item) => {
+        return sum + ((item && item.likes) ? item.likes : 0)
+    }, 0)
+}
   
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
