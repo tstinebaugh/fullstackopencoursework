e766a2e9329bd5886f127473ac50f9de37d5d41a
Travis Stinebaugh <57374519+tstinebaugh@users.noreply.github.com>
2022-12-24 15:07:59 -0800

    4.5

--- part4/bloglist/tests/list_helper.test.js
+++ part4/bloglist/tests/list_helper.test.js
@@ -60,3 +60,60 @@ describe('total likes', () => {
     expect(result).toBe(15)
   })
 })
+
+describe('favorite blog', () => {
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  const multipleBlogs = [
+    ...listWithOneBlog,
+    {
+      title: 'post 2',
+      author: 'bob',
+      url: 'google.com',
+      likes: 42
+    },
+    {
+      title: 'post 3',
+      author: 'smith',
+      url: 'google.com',
+      likes: 1
+    }
+  ]
+
+  test('null blogs', () => {
+    const result = listHelper.favoriteBlog(null)
+    expect(result).toBe(undefined)
+  })
+
+  test('no blogs', () => {
+    const result = listHelper.favoriteBlog([])
+    expect(result).toBe(undefined)
+  })
+
+  test('one blog', () => {
+    const result = listHelper.favoriteBlog(listWithOneBlog)
+    expect(result).toEqual({
+      title: listWithOneBlog[0].title,
+      author: listWithOneBlog[0].author,
+      likes: listWithOneBlog[0].likes
+    })
+  })
+
+  test('multiple blogs', () => {
+    const result = listHelper.favoriteBlog(multipleBlogs)
+    expect(result).toEqual({
+      title: multipleBlogs[1].title,
+      author: multipleBlogs[1].author,
+      likes: multipleBlogs[1].likes
+    })
+  })
+})
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -9,8 +9,25 @@ const totalLikes = (blogs) => {
         return sum + ((item && item.likes) ? item.likes : 0)
     }, 0)
 }
+
+const favoriteBlog = (blogs) => {
+    let maxLikes = -1
+    let highestBlog
+    blogs && blogs.forEach(blog => {
+        if (blog && blog.likes && blog.likes > maxLikes) {
+            maxLikes = blog.likes
+            highestBlog = {
+                title: blog.title,
+                author: blog.author,
+                likes: blog.likes
+            }
+        }
+    })
+    return highestBlog
+}
   
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
