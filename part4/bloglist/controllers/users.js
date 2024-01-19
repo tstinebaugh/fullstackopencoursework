cd0e6892e71443f97c53220aa89af27fb9c3c58c
Travis Stinebaugh <57374519+tstinebaugh@users.noreply.github.com>
2024-01-19 15:06:31 -0800

    4.15

---
+++ part4/bloglist/controllers/users.js
@@ -0,0 +1,37 @@
+const bcrypt = require('bcrypt')
+const usersRouter = require('express').Router()
+const User = require('../models/user')
+
+const minPwLen = 3
+
+usersRouter.post('/', async (request, response) => {
+  const { username, name, password } = request.body
+
+  if (!password || password.length <= minPwLen) {
+    return response.status(401).json({
+        error: `password must be greater than ${minPwLen} characters`
+      })
+  }
+
+  const saltRounds = 10
+  const passwordHash = await bcrypt.hash(password, saltRounds)
+
+  const user = new User({
+    username,
+    name,
+    passwordHash,
+  })
+
+  const savedUser = await user.save()
+
+  response.status(201).json(savedUser)
+})
+
+usersRouter.get('/', async (request, response) => {
+    const users = await User
+        .find({})
+    
+    response.json(users)
+})
+
+module.exports = usersRouter
