462039456a1c0076ef295f232536642ea37a8fb0
Travis Stinebaugh <57374519+tstinebaugh@users.noreply.github.com>
2024-01-19 17:01:47 -0800

    4.21

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -12,6 +12,10 @@ blogRouter.get('/', async (_request, response) => {
 blogRouter.post('/', async (request, response) => {
     const blog = new Blog(request.body)
 
+    if (!request.token) {
+        return response.status(401).json({ error: 'token missing' })
+    }
+
     const decodedToken = jwt.verify(request.token, process.env.SECRET)
     if (!decodedToken.id) {
       return response.status(401).json({ error: 'token invalid' })
@@ -27,6 +31,23 @@ blogRouter.post('/', async (request, response) => {
 })
 
 blogRouter.delete('/:id', async (request, response, next) => {
+    if (!request.token) {
+        return response.status(401).json({ error: 'token missing' })
+    }
+
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'token invalid' })
+    }
+
+    const user = await User.findById(decodedToken.id)
+
+    const blog = await Blog.findById(request.params.id).populate('user')
+    
+    if (blog.user.id.toString() !== user.id.toString()) {
+        return response.status(401).json({ error: 'invalid token for deleting this blog' })
+    }
+
     await Blog.findByIdAndDelete(request.params.id)
     response.status(204).end()
 })
