3e632bf1fb4f5fdd9684b642cf70bad5feeaaf5b
Travis Stinebaugh <57374519+tstinebaugh@users.noreply.github.com>
2024-01-19 16:06:28 -0800

    4.17

--- part4/bloglist/controllers/blog.js
+++ part4/bloglist/controllers/blog.js
@@ -1,14 +1,20 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogRouter.get('/', async (_request, response) => {
-    const blogs = await Blog.find({})
+    const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
     response.json(blogs)
 })
 
 blogRouter.post('/', async (request, response) => {
     const blog = new Blog(request.body)
+    // TODO: Remove this in 4.19
+    const user = await User.findOne()
+    blog.user = user.id
     const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
     response.status(201).json(savedBlog)
 })
 
