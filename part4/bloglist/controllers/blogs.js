c1d92bb505788fe0feff6a4e51f497502d8313b5
Travis Stinebaugh <57374519+tstinebaugh@users.noreply.github.com>
2024-01-19 16:27:00 -0800

    imports & 4.19

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,6 +1,16 @@
+const jwt = require('jsonwebtoken')
+
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.startsWith('Bearer ')) {
+        return authorization.replace('Bearer ', '')
+    }
+    return null
+}
 
 blogRouter.get('/', async (_request, response) => {
     const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
@@ -9,8 +19,14 @@ blogRouter.get('/', async (_request, response) => {
 
 blogRouter.post('/', async (request, response) => {
     const blog = new Blog(request.body)
-    // TODO: Remove this in 4.19
-    const user = await User.findOne()
+
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'token invalid' })
+    }
+
+    const user = await User.findById(decodedToken.id)
+    
     blog.user = user.id
     const savedBlog = await blog.save()
     user.blogs = user.blogs.concat(savedBlog._id)
