e0b30f99635ae4146f43ded25b9ae5a38c7f1762
Travis Stinebaugh <57374519+tstinebaugh@users.noreply.github.com>
2024-02-25 11:18:59 -0800

    6.20

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import { useQuery, useMutation, useQueryClient  } from '@tanstack/react-query'
+
+import AnecdoteForm from './components/AnecdoteForm'
+import Notification from './components/Notification'
+import { getAnecdotes } from './services/request'
+
+const App = () => {
+
+  const handleVote = (anecdote) => {
+    console.log('vote')
+  }
+
+  const result = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: getAnecdotes,
+    refetchOnWindowFocus: false
+  })
+
+  console.log(JSON.parse(JSON.stringify(result)))
+
+  if ( result.isLoading ) {
+    return <div>loading data...</div>
+  }
+
+  if (result.isError) {
+    return <div>Error: {result.error.message}</div>
+  }
+
+  const anecdotes = result.data
+
+  return (
+    <div>
+      <h3>Anecdote app</h3>
+    
+      <Notification />
+      <AnecdoteForm />
+    
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => handleVote(anecdote)}>vote</button>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default App
